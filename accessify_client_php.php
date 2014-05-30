6c5ae09589888aec2e42c8d2478abd86d5208c84
Nick Freear <nfreear@yahoo.co.uk>
2014-05-31 00:03:04 +0100

    Improve glue Javascript, based on "wp-accessify" WP plugin
    * Plus, extra public methods required by "wp-accessify" etc.

--- accessify_client_php.php
+++ accessify_client_php.php
@@ -7,7 +7,7 @@
 class Accessify_Client_Php {
 
   const API_URL  = '//accessifywiki.appspot.com/';  //No "http:"
-  const WIKI_URL = 'http://accessify.wikia.com/wiki/WordPress';
+  const WIKI_URL = 'http://accessify.wikia.com/wiki/';
   const CACHE_JS = 'cache/accessify-site-fixes.js';
   const APP_ID   = 'accessify-client-php';
   const FIX_OPT  = '&min=1&callback=__accessify_IPG&app=';
@@ -15,10 +15,12 @@ class Accessify_Client_Php {
 
   protected $site_id = '';
   protected $mode_cache = FALSE;
+  protected $app_id;
 
 
-  public function __construct() {
-  
+  public function __construct($site_id = NULL, $app_id = NULL) {
+    $this->site_id = $site_id;
+    $this->app_id = $app_id ? $app_id : self::APP_ID;
   }
 
 
@@ -28,38 +30,68 @@ class Accessify_Client_Php {
   protected function print_glue_javascript() {
     ?>
 
+  AC5U = window.AC5U || {};
+
   function __accessify_IPG(fixes) {
     "use strict";
 
-    var res,
+    var G = AC5U,
+      L = document.location,
       pat = /debug/,
-      L = document.location;
+      debug = G.debug || L.search.match(pat) || L.hash.match(pat);
 
     function log(s) {
-      if (typeof console !== "undefined" && (L.search.match(pat) || L.hash.match(pat))) {
+      window.console && debug &&
         console.log(arguments.length > 1 ? arguments : s);
-      }
     }
 
-    log("AccessifyHTML5");
+    if (G.result) {
+      return log("AccessifyHTML5: already run");
+    }
 
-    res = AccessifyHTML5(false, fixes);
+    log("AccessifyHTML5: run");
 
-    log(res);
+    G.result = AccessifyHTML5(false, fixes);
+
+    log(G.result);
   }
 <?php
   }
 
+
+  public function print_fix_test_scripts() {
+    ?>
+    <script src="<?php echo $this->lib_url() ?>" id="accessify-js"></script>
+    <script><?php $this->print_glue_javascript() ?></script>
+    <script src="<?php echo $this->fix_url() ?>"></script>
+<?php
+  }
+
+
+  public function debug_config_scripts() {
+    ?><script>AC5U = { debug: true }</script>
+<?php
+  }
+
+  public function wiki_url($page = NULL) {
+    return self::WIKI_URL . str_replace(' ', '_', $page);
+  }
+
   public function is_valid_site_id( $site_id ) {
-    return $site_id && preg_match( '/^Fix:[\w\_]+$/', $site_id );
+    return $site_id && preg_match( self::SITE_ID_REGEX, $site_id );
   } 
+
 
   protected function lib_url() {
     return self::API_URL .'browser/js/accessifyhtml5.js';
   }
 
-  protected function fix_url() {
-    return self::API_URL .'fix?q='. $this->site_id . self::FIX_OPT . self::APP_ID;
+  public function fix_url() {
+    return self::API_URL .'fix?q='. $this->site_id . self::FIX_OPT . $this->app_id;
+  }
+
+  protected function build_cache_fix_javascript() {
+    // TODO: Implement cached fix JS.
   }
 
 }
